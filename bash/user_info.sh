7ad43ea12c2f3720ee3a83b00f7c83fabb6e5c13
Xavier Davis <xvrtechxplorations@gmail.com>
2024-11-13 17:43:56 -0600

    user-info

--- bash/user_info.sh
+++ bash/user_info.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# create variables to print in script
+#can use command outputs in variable
 user=$(whoami)
 whereami=$(pwd)
 date=$(date)
 host=$(hostname)
 
+# use $ to indicate variable
 echo "You are currently loggied in as $user on host $host"
 echo "You current directory is $whereami"
 echo "The date is $date"
